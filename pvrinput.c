7f025769bf1b1cbaf36ff8e0c08d6d1f70b21ed3
VDR user <vdr@vmyavdr.gitarren.local>
2010-04-03 16:01:27 +0200

    Increase version for next rc-release

--- pvrinput.c
+++ pvrinput.c
@@ -6,7 +6,7 @@
 #endif
 #endif
 
-static const char *VERSION        = "2010-04-01-rc1";
+static const char *VERSION        = "2010-04-03-rc1";
 static const char *DESCRIPTION    = tr("use Hauppauge PVR as input device");
 static const char *MAINMENUENTRY  = tr("PVR picture settings");
 
