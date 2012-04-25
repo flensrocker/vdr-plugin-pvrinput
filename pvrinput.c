383ac73e940a75813d400cbc44c430b561d98452
Lars Hanisch <dvb@flensrocker.de>
2012-04-25 19:32:40 +0200

    correct some file modes

--- pvrinput.c
+++ pvrinput.c
@@ -6,7 +6,7 @@
 #endif
 #endif
 
-static const char *VERSION        = "2011-08-18";
+static const char *VERSION        = "2012-04-25";
 static const char *DESCRIPTION    = tr("use Hauppauge PVR as input device");
 static const char *MAINMENUENTRY  = tr("PVR picture settings");
 
